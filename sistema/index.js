8e036a71e57883a7be1fabc8635b8633f130710d
Silvio Coelho <conejofoz@gmail.com>
2021-12-30 14:00:52 -0300

    middlewares

--- sistema/index.js
+++ sistema/index.js
@@ -1,5 +1,12 @@
 const express=require('express')
+const morgan=require('morgan')
+const cors=require('cors')
+
 const app=express()
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.set('port', process.env.PORT || 3000)
 
