8bfa5659bcf5307494464701e40dbd024e9d10e2
Silvio Coelho <conejofoz@gmail.com>
2021-12-30 15:43:04 -0300

    Atualizando para ES6 e arquivos estáticos

--- sistema/index.js
+++ sistema/index.js
@@ -1,12 +1,20 @@
+/*
 const express=require('express')
 const morgan=require('morgan')
 const cors=require('cors')
+*/
+
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
 
 const app=express()
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('port', process.env.PORT || 3000)
 
