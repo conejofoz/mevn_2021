c67509bcb2a28d9a44bf3d7cbb8ee541b29273a9
conejofoz <conejofoz@gmail.com>
2022-01-11 07:52:29 -0300

    front login vue

--- sistemavue/src/router.js
+++ sistemavue/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Categoria from './components/Categoria.vue'
+import Login from './components/Login.vue'
 
 Vue.use(Router)
 
@@ -20,12 +21,10 @@ export default new Router({
       component: Categoria
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    
   ]
 })
