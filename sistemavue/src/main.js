f8df32600f92e6fb37b6ee5d240345ae1b504064
conejofoz <conejofoz@gmail.com>
2022-01-18 23:00:58 -0300

    sistema finalizado

--- sistemavue/src/main.js
+++ sistemavue/src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL='http://127.0.0.1:3000/api/'
+axios.defaults.baseURL='http://192.168.0.109:3000/api/'
 
 new Vue({
   router,
